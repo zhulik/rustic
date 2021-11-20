9c5fba645ffce7b8a77d02427b64819ce35188d0
Gleb Sinyavskiy <zhulik.gleb@gmail.com>
2021-11-21 00:17:34 +0100

    Support check without config

--- lib/rustic/command_builders/check.rb
+++ lib/rustic/command_builders/check.rb
@@ -7,18 +7,16 @@ class Rustic::CommandBuilders::Check
 
   def build
     config = @config.check_config
-    raise Rustic::CommandBuilder::MissingConfigError, "Command `check` misses it's configuration" if config.nil?
-
     [
       "check",
-      config.check_unused ? "--check-unused" : nil,
+      config&.check_unused ? "--check-unused" : nil,
       read_data_subset(config),
-      config.with_cache ? "--with-cache" : nil
+      config&.with_cache ? "--with-cache" : nil
     ].compact
   end
 
   def read_data_subset(config)
-    return nil if config.read_data_subset.nil?
+    return nil if config.nil? || config.read_data_subset.nil?
     return "--read-data" if config.read_data_subset == 100
 
     "--read-data-subset=#{config.read_data_subset}%"
